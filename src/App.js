9f8a3692394b742de7d935c2ab86545892ff690c
robbied112 <robbied112@gmail.com>
2025-03-21 15:37:25 -0700

    Updated App.js to use Routes instead of Switch for react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import WinePricingCalculator from './components/WinePricingCalculator';
 import ReverseWinePricingCalculator from './components/ReverseWinePricingCalculator';
 
@@ -6,14 +7,17 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <Link to="/">Forward Calculator</Link> | <Link to="/reverse">Reverse Calculator</Link>
+        <nav className="p-4 bg-gray-100">
+          <Link to="/" className="mr-4 text-blue-600 hover:underline">Forward Calculator</Link>
+          <Link to="/reverse" className="text-blue-600 hover:underline">Reverse Calculator</Link>
         </nav>
-        <Switch>
-          <Route exact path="/" component={WinePricingCalculator} />
-          <Route path="/reverse" component={ReverseWinePricingCalculator} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<WinePricingCalculator />} />
+          <Route path="/reverse" element={<ReverseWinePricingCalculator />} />
+        </Routes>
       </div>
     </Router>
   );
 }
+
+export default App;
