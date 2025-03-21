8d500df3672ebde14f3b7dbd0cc1a3351e81005d
robbied112 <robbied112@gmail.com>
2025-03-21 14:24:45 -0700

    Updated App.js to include routing for Reverse Calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import WinePricingCalculator from './components/WinePricingCalculator';
+import ReverseWinePricingCalculator from './components/ReverseWinePricingCalculator';
 
 function App() {
   return (
-    <div className="App">
-      <WinePricingCalculator />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Forward Calculator</Link> | <Link to="/reverse">Reverse Calculator</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={WinePricingCalculator} />
+          <Route path="/reverse" component={ReverseWinePricingCalculator} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
